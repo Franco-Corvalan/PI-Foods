74781e4faa46f32c3c6565f2917a88fe29d6cf97
Franco-Corvalan <129094317+Franco-Corvalan@users.noreply.github.com>
2024-02-21 20:42:20 -0300

    hero

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
