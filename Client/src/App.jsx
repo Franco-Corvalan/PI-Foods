946d4048682bae0d61ff0d07d1fd38700450a2ac
Franco-Corvalan <129094317+Franco-Corvalan@users.noreply.github.com>
2024-02-22 16:40:56 -0300

    Foods

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,15 +2,16 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HeadlineCards from "./components/HeadlineCards"
+import Foods from "./components/Foods"
 
 function App() {
   
-
   return (
     <div>
       <Navbar/>
       <Hero/>
       <HeadlineCards/>
+      <Foods/>
     </div>
   )
 }
