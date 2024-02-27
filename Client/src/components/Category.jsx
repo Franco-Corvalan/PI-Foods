09f8796ca34d7ab7c02ee0a32a804967221c10a8
Franco-Corvalan <129094317+Franco-Corvalan@users.noreply.github.com>
2024-02-27 14:42:36 -0300

    ajustes

--- Client/src/components/Category.jsx
+++ Client/src/components/Category.jsx
@@ -5,7 +5,7 @@ function Category() {
   
   return (
     <div className="max-w-[1640px] m-auto px-4 py-12">
-      <h1 className="text-orange-600 font-bold text-4xl text-center">Top Rated Menu Items </h1>
+      <h1 className="text-orange-600 font-bold text-4xl text-center">Categorys</h1>
 
       {/* Categorias */}
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
--- Client/src/components/Foods.jsx
+++ Client/src/components/Foods.jsx
@@ -26,7 +26,7 @@ function Foods() {
   return (
     
     <div className="max-w-[1640px] m-auto px-4 py-12">
-      <h1 className="text-orange-600 font-bold text-4xl text-center">Top Rater Menu Items</h1>
+      <h1 className="text-orange-600 font-bold text-4xl text-center">Menu</h1>
 
       {/* Filters */}
       <div className="flex flex-col lg:flex-row justify-between">
